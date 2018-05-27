6e3cb2faa4ee2ba926e6899b0687ddc42220fc66
JeanWayne <jeancharbonnier@hotmail.com>
2018-05-27 15:07:47 +0200

    Added Model for v2

--- ImageClassifier/v2/PyTorchHelper.py
+++ ImageClassifier/v2/PyTorchHelper.py
@@ -4,6 +4,30 @@ from torch.autograd import Variable
 from torchvision import models
 from torchvision.transforms import transforms
 
+names=['composite','diagramm','imaging','photos','vis']
+
+def getLabelName(i):
+    if i==0:
+        return "composite"
+    if i==1:
+        return "diagramm"
+    if i==2:
+        return "imaging"
+    if i==3:
+        return "photos"
+    if i==4:
+        return "vis"
+
+def getImageClass(path):
+    img = Image.open(path)
+    img_tensor = preprocess(img)
+    img_tensor.unsqueeze_(0)
+    input= Variable(img_tensor).type(torch.FloatTensor).cuda()
+    output = model(input)
+    _, preds = torch.max(output.data, 1)
+
+    return getLabelName(preds[0])
+
 use_gpu = torch.cuda.is_available()
 
 model_ft = models.resnet18(pretrained=False)
@@ -29,22 +53,14 @@ preprocess = transforms.Compose([
     transforms.ToTensor(),
     normalize
 ])
-photos=["/home/jean/Image_class_v2/val/photos/10.1100_2011_545421---1.jpg","/home/jean/Image_class_v2/val/photos/10.1100_2012_106429---5.jpg","/home/jean/Image_class_v2/val/photos/10.1100_2012_481584---1.jpg"]
-diagramm=["/home/jean/Image_class_v2/val/diagram/10.1093_ecam_nen040---3.jpg","/home/jean/Image_class_v2/val/diagram/10.1093_ecam_nen043---4.jpg","/home/jean/Image_class_v2/val/diagram/10.1093_ecam_nen061---3.jpg"]
-composite=["/home/jean/Image_class_v2/val/composite/10.1093_ecam_nep188---1.jpg","/home/jean/Image_class_v2/val/composite/10.1155_2011_346413---0.jpg","/home/jean/Image_class_v2/val/composite/10.1155_2010_635294---3.jpg"]
-imaging=["/home/jean/Image_class_v2/val/imaging/10.1100_2012_501751---12.jpg","/home/jean/Image_class_v2/val/imaging/10.1100_2012_537973---6.jpg","/home/jean/Image_class_v2/val/imaging/10.1155_2010_513461---6.jpg"]
-names=['composite','diagramm','imaging','photos','vis']
-kl=0
-for k in [photos,diagramm,composite,imaging]:
-    print("_____________")
-    for i in k:
-        img = Image.open(i)
-        img_tensor = preprocess(img)
-        img_tensor.unsqueeze_(0)
-        input= Variable(img_tensor).type(torch.FloatTensor).cuda()
-        output = model(input)
-        _, preds = torch.max(output.data, 1)
-        print(str(names[kl])+"   "+str(output)+"      "+str(preds))
-        #print(preds)
-    print("_____________")
-    kl+=1
+#photos=["/home/jean/Image_class_v2/val/photos/10.1100_2011_545421---1.jpg","/home/jean/Image_class_v2/val/photos/10.1100_2012_106429---5.jpg","/home/jean/Image_class_v2/val/photos/10.1100_2012_481584---1.jpg"]
+#diagramm=["/home/jean/Image_class_v2/val/diagram/10.1093_ecam_nen040---3.jpg","/home/jean/Image_class_v2/val/diagram/10.1093_ecam_nen043---4.jpg","/home/jean/Image_class_v2/val/diagram/10.1093_ecam_nen061---3.jpg"]
+#composite=["/home/jean/Image_class_v2/val/composite/10.1093_ecam_nep188---1.jpg","/home/jean/Image_class_v2/val/composite/10.1155_2011_346413---0.jpg","/home/jean/Image_class_v2/val/composite/10.1155_2010_635294---3.jpg"]
+#imaging=["/home/jean/Image_class_v2/val/imaging/10.1100_2012_501751---12.jpg","/home/jean/Image_class_v2/val/imaging/10.1100_2012_537973---6.jpg","/home/jean/Image_class_v2/val/imaging/10.1155_2010_513461---6.jpg"]
+#kl=0
+#for k in [photos,diagramm,composite,imaging]:
+ #   print("_____________")
+  #  for i in k:
+
+
+
--- ImageClassifier/v2/main.py
+++ ImageClassifier/v2/main.py
@@ -1,9 +1,18 @@
 # import ImageClassifier.v2.MongoHelper
 from ImageClassifier.v2.MongoHelper import getUnclassifiedImages, updateImageWithLabel, getPathForImage, unsetLabels
+from ImageClassifier.v2.PyTorchHelper import getImageClass
 
 unsetLabels()
 
-# for i in getUnclassifiedImages(limit=10):
-# 	print(getPathForImage(i['_id']))
-# 	# classify
-# 	updateImageWithLabel(i['_id'], "test2")
+maxRun=10
+iterLimit=10
+
+for j in maxRun:
+    for i in getUnclassifiedImages(limit=iterLimit):
+        #print(getPathForImage(i['_id']))
+        path=getPathForImage(i['_id'])
+        label=getImageClass(path)
+        updateImageWithLabel(i['_id'], "label")
+    print("{0} Images Processed, starting next Query".format(iterLimit))
+print("Completed {0} x {1} runs".format(maxRun,iterLimit))
+
